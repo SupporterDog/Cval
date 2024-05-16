976da6b005de395da0c2aabed516713d9270223f
ChungEunho <151016399+ChungEunho@users.noreply.github.com>
2024-05-16 22:39:04 +0900

    Update CVALSAEKKYA.c

--- CVALSAEKKYA.c
+++ CVALSAEKKYA.c
@@ -2,37 +2,34 @@
 #include <wiringPiI2C.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     int _device;
     int _addr;
 } YB_Pcb_Car;
 
-// I2C 장치 초기화
-void initialize_i2c_device(YB_Pcb_Car* car, int address) {
+void get_i2c_device(YB_Pcb_Car* car, int address, int i2c_bus) {
     car->_addr = address;
-    car->_device = wiringPiI2CSetup(car->_addr);
+    car->_device = wiringPiI2CSetup(car->_addr); // 주어진 I2C 주소로 I2C 장치 초기화
     if (car->_device == -1) {
         printf("Failed to initialize I2C device\n");
         exit(1);
     }
 }
 
-// 8비트 데이터 쓰기
-void write_u8(YB_Pcb_Car* car, int reg, int data) {
-    if (wiringPiI2CWriteReg8(car->_device, reg, data) == -1) {
+void write_u8(YB_Pcb_Car* car, int data) {
+    if (wiringPiI2CWrite(car->_device, data) == -1) {
         printf("write_u8 I2C error\n");
     }
 }
 
-// 레지스터에 쓰기
 void write_reg(YB_Pcb_Car* car, int reg) {
     if (wiringPiI2CWrite(car->_device, reg) == -1) {
         printf("write_reg I2C error\n");
     }
 }
 
-// 배열 형태의 데이터를 레지스터 주소에 연속적으로 쓰는 함수
 void write_array(YB_Pcb_Car* car, int reg, int* data, int length) {
     for (int i = 0; i < length; ++i) {
         if (wiringPiI2CWriteReg8(car->_device, reg + i, data[i]) == -1) {
@@ -42,33 +39,42 @@ void write_array(YB_Pcb_Car* car, int reg, int* data, int length) {
     }
 }
 
-// 모터 제어
-void control_motors(YB_Pcb_Car* car, int* speeds) {
+void Ctrl_Car(YB_Pcb_Car* car, int* speed) {
     int reg = 0x01;
-    write_array(car, reg, speeds, 4);
+    write_array(car, reg, speed, 4);
 }
 
-// 주행 시작
-void start_driving(YB_Pcb_Car* car, int* speeds) {
-    control_motors(car, speeds);
+void Car_Run(YB_Pcb_Car* car, int* speed) {
+    Ctrl_Car(car, speed);
 }
 
-// 주행 종료
-void stop_driving(YB_Pcb_Car* car) {
-    int reg = 0x02;
-    write_u8(car, reg, 0x00);
+void Car_Stop(YB_Pcb_Car* car) {
+    int speed[4] = {0, 0, 0, 0};
+    Ctrl_Car(car, speed);
+}
+
+void Car_Left(YB_Pcb_Car* car, int* speed) {
+    // 모터 연결 방향에 따라 속도 조정이 필요할 수 있음
+    int adjusted_speed[4] = {speed[0], speed[1], -speed[2], -speed[3]};
+    Ctrl_Car(car, adjusted_speed);
+}
+
+void Car_Right(YB_Pcb_Car* car, int* speed) {
+    // 모터 연결 방향에 따라 속도 조정이 필요할 수 있음
+    int adjusted_speed[4] = {-speed[0], -speed[1], speed[2], speed[3]};
+    Ctrl_Car(car, adjusted_speed);
 }
 
 int main() {
     wiringPiSetup();
     YB_Pcb_Car car;
-    initialize_i2c_device(&car, 0x16);
+    get_i2c_device(&car, 0x16, 1);
 
-    int speeds[4] = { 100, 100, 100, 100 };
+    int speed[4] = { 100, 100, 100, 100 };
 
-    start_driving(&car, speeds);
+    Car_Run(&car, speed);
     delay(2000); // 2초 동안 주행
-    stop_driving(&car);
+    Car_Stop(&car);
 
     return 0;
 }
