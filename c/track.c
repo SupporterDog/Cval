461c1bf64ae9f45bbc20577d99a0e44b7ecced66
SupporterDog <68847588+SupporterDog@users.noreply.github.com>
2024-06-03 22:53:48 +0900

    Update track.c

--- c/track.c
+++ c/track.c
@@ -156,7 +156,7 @@ void line_following(YB_Pcb_Car* car) {
             break;
         case 0b1001:  // (H L L H) : 앞으로 직진
 
-            Car_Run(car, 70, 70);
+            Car_Run(car, 60, 60);
             delay(5);
             left1 = read_sensor(SENSOR_LEFT1);
             left2 = read_sensor(SENSOR_LEFT2);
