3121cee52f604a2e7da0e134f2aef41b2c5ba050
Hanungjae <168642451+Hanungjae@users.noreply.github.com>
2024-06-03 22:09:07 +0900

    Update client.c

--- c/client.c
+++ c/client.c
@@ -18,7 +18,7 @@ void* receiveUpdates(void* arg) {
         printf("my Index: %d\n",me_index);
         updateGlobalVariables(&dgist,sock);
         
-        usleep(100000);
+        usleep(50000);
     }
     return NULL;
 }
