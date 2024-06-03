e5a14cd7b1f371e248b196e9e05a6950a1037ca3
SupporterDog <68847588+SupporterDog@users.noreply.github.com>
2024-06-03 21:33:58 +0900

    Update track.c

--- c/track.c
+++ c/track.c
@@ -70,8 +70,8 @@ void perform_car_run_and_turn(YB_Pcb_Car* car, int* sensor_state, int control) {
     else if (control == r_spin) {
         Car_Run(car, 60, 60);
         delay(150);
-        Car_Right(car, 100, 100);
-        delay(120);
+        Car_Right(car, 60, 60);
+        delay(500);
         while ((*sensor_state & 0b0100) != 0b0100) {
             Car_Right(car, 60, 60);
             *sensor_state = *sensor_state + (read_sensor(SENSOR_LEFT2) << 2);
@@ -82,8 +82,8 @@ void perform_car_run_and_turn(YB_Pcb_Car* car, int* sensor_state, int control) {
     else if (control == l_spin) {
         Car_Run(car, 60, 60);
         delay(150);
-        Car_Left(car, 100, 100);
-        delay(120);
+        Car_Left(car, 60, 60);
+        delay(500);
         while ((*sensor_state & 0b0010) != 0b0010) {
             Car_Left(car, 60, 60);
             *sensor_state = *sensor_state + (read_sensor(SENSOR_RIGHT2) << 1);
@@ -94,8 +94,8 @@ void perform_car_run_and_turn(YB_Pcb_Car* car, int* sensor_state, int control) {
     else {
         Car_Run(car, 60, 60);
         delay(150);
-        Car_Right(car, 100, 100);
-        delay(300);
+        Car_Right(car, 60, 60);
+        delay(1800);
         while ((*sensor_state & 0b0100) != 0b0100) {
             Car_Right(car, 60, 60);
             *sensor_state = *sensor_state + (read_sensor(SENSOR_LEFT2) << 2);
